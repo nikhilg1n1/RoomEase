b8dfd4eba17893847131b0ba1f83e38a08933df4
NIkhil <nikhilghinamine@gmail.com>
2025-11-01 22:20:47 +0530

    check commit

---
+++ src/main/java/com/roomeaseauth/RoomEaseAuthApplication.java
@@ -0,0 +1,13 @@
+package com.roomeaseauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomEaseAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomEaseAuthApplication.class, args);
+    }
+
+}
