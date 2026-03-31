590dcc918b3521da2a350d9d683ffcb3668bd583
NIkhil <nikhilghinamine@gmail.com>
2026-04-01 00:31:13 +0530

    Added the SecurityTest in project

---
+++ src/test/java/com/roomease/RoomSecurityTest.java
@@ -0,0 +1,53 @@
+package com.roomease;
+
+import com.roomease.Auth.JwtFilter;
+import com.roomease.Auth.JwtUtils;
+import com.roomease.Config.SecurityConfig;
+import com.roomease.Controller.RoomController;
+import com.roomease.Repository.*;
+import com.roomease.Services.ListRoomService;
+import com.roomease.Services.RentRoomService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(value = RoomController.class)
+public class RoomSecurityTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean private RentRoomService rentRoomService;
+    @MockitoBean private ListRoomRepo listRoomRepo;
+    @MockitoBean private ListRoomService listRoomService;
+    @MockitoBean private RoomImageRepo roomImageRepo;
+    @MockitoBean private OauthUserRepo oauthUserRepo;
+    @MockitoBean private BookingRepo bookingRepo;
+    @MockitoBean private JwtUtils jwtUtils;
+    @MockitoBean private JwtFilter jwtFilter;
+    @MockitoBean private ReviewRepo reviewRepo;
+
+    @Test
+    void getAllRooms_withoutAuth_shouldReturn401() throws  Exception{
+        mockMvc.perform(get("/v1/rooms"))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    void searchRooms_withoutAuth_shouldReturn401() throws  Exception{
+        mockMvc.perform(get("/v1/searchRooms").param("query","Pune"))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    void getRoomDescription_withoutAuth_shouldReturn401() throws Exception{
+        mockMvc.perform(get("/v1/description/1"))
+                .andExpect(status().is3xxRedirection());
+    }
+
+}
