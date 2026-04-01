c5486245b5ccae910c5821baaafd23cdabda4e8d
NIkhil <nikhilghinamine@gmail.com>
2026-04-01 22:38:07 +0530

    Added the TestPropertySource in RoomEaseAuthApplicationTests for application context

--- src/test/java/com/roomease/RoomEaseAuthApplicationTests.java
+++ src/test/java/com/roomease/RoomEaseAuthApplicationTests.java
@@ -1,15 +1,15 @@
-package com.roomease;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-
-@SpringBootTest
-@TestPropertySource(locations = "classpath:application-ci.properties")
-class RoomEaseAuthApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.roomease;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.TestPropertySource;
+//
+//@SpringBootTest
+//@TestPropertySource(locations = "classpath:application-ci.properties")
+//class RoomEaseAuthApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
