6a3a40fe73b670cec0104326bbffd7d94641aafd
NIkhil <nikhilghinamine@gmail.com>
2026-05-06 00:12:54 +0530

    make changes in Redis config file added SSL before that getting 500 error

--- src/main/java/com/roomease/Config/RedisConfig.java
+++ src/main/java/com/roomease/Config/RedisConfig.java
@@ -1,11 +1,15 @@
 package com.roomease.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
@@ -19,6 +23,36 @@ import java.time.Duration;
 @EnableCaching
 //@EnableRedisHttpSession
 public class RedisConfig {
+    @Value("${REDIS_HOST}")
+    private String redisHost;
+
+    @Value("${REDIS_PORT}")
+    private int redisPort;
+
+    @Value("${REDIS_PASSWORD}")
+    private String redisPassword;
+
+    @Value("${spring.data.redis.ssl.enabled:false}")
+    private boolean sslEnabled;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory(){
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisHost);
+        config.setPort(redisPort);
+        config.setPassword(redisPassword);
+        config.setUsername("default");
+
+        LettuceClientConfiguration clientConfig;
+
+        if(sslEnabled){
+            clientConfig = LettuceClientConfiguration.builder().useSsl().disablePeerVerification().build();
+        }
+        else {
+            clientConfig = LettuceClientConfiguration.builder().build();
+        }
+        return new LettuceConnectionFactory(config,clientConfig);
+    }
 
     @Bean
     public RedisTemplate<String,Object> redisTemplate (RedisConnectionFactory connectionFactory){
